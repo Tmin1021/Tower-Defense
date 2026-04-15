9e5cbdb0919aa735bb01aaab972e519d9622c785
Tmin1021 <128274812+Tmin1021@users.noreply.github.com>
2026-04-16 03:05:56 +0700

    Update Wave Logic

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,38 +1,43 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 3f;
-    [SerializeField] Path currentPath;
+    [SerializeField] private EnemyData data;
+    private Path _currentPath;
     private Vector3 _targetPosition;
     private int _currentWaypoint;
+    public static event Action<EnemyData> OnEnemyReachedEnd;
 
     void Awake()
     {
-        currentPath = GameObject.Find("Path1").GetComponent<Path>();
+        _currentPath = GameObject.Find("Path1").GetComponent<Path>();
     }
 
     void OnEnable()
     {
         _currentWaypoint = 0;
-        _targetPosition = currentPath.GetPosition(_currentWaypoint);
+        _targetPosition = _currentPath.GetPosition(_currentWaypoint);
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, moveSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, data.speed * Time.deltaTime);
 
         float relativeDistance = (transform.position - _targetPosition).magnitude;
 
         if(relativeDistance < 0.1f)
         {    
-            if(_currentWaypoint < currentPath.Waypoints.Length - 1)
+            if(_currentWaypoint < _currentPath.Waypoints.Length - 1)
             {
                 _currentWaypoint++;
-                _targetPosition = currentPath.GetPosition(_currentWaypoint);
+                _targetPosition = _currentPath.GetPosition(_currentWaypoint);
             }
-            else gameObject.SetActive(false);
+            else // reached last waypoint
+            {
+                OnEnemyReachedEnd?.Invoke(data);
+                gameObject.SetActive(false);
+            } 
         }
-
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,27 +1,93 @@
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField] WaveData[] waves;
+    private int _currentWaveIndex = 0;
+    private WaveData _currentWave => waves[_currentWaveIndex];
     private float _spawnTimer;
-    private float _spawnInterval  = 1f;
+    private int _spawnCounter = 0;
+    private int _enemiesRemoved = 0;
+
+    private float _timeBetweenWaves = 2f;
+    private float _waveCooldown;
+    private bool _isBetweenWaves = false;
     // public GameObject Prefab;
-    public ObjectPooler pool;
+    [SerializeField] private ObjectPooler orcPool;
+    [SerializeField] private ObjectPooler dragonPool;
+    [SerializeField] private ObjectPooler kaijuPool;
+
+    private Dictionary<EnemyType, ObjectPooler> _poolDictionary;
+
+    void Awake()
+    {
+        _poolDictionary = new Dictionary<EnemyType, ObjectPooler>()
+        {
+            {EnemyType.Orc, orcPool},
+            {EnemyType.Dragon, dragonPool},
+            {EnemyType.Kaiju, kaijuPool}
+        };
+    }
+
+    void OnEnable()
+    {
+        Enemy.OnEnemyReachedEnd += HandleEnemyReachedEnd;
+    }
+
+    void OnDisable()
+    {
+        Enemy.OnEnemyReachedEnd -= HandleEnemyReachedEnd;
+    }
 
     void Update()
     {
-        _spawnTimer -= Time.deltaTime;
-        if(_spawnTimer < 0)
+        if(_isBetweenWaves)
         {
-            _spawnTimer = _spawnInterval;
-            SpawnEnemy();
+            _waveCooldown -= Time.deltaTime;
+            if(_waveCooldown <= 0)
+            {
+                _currentWaveIndex = (_currentWaveIndex + 1) % waves.Length; // continue to next wave 
+                _spawnCounter = 0;
+                _enemiesRemoved = 0;
+                _spawnTimer = 0; // immediately spawn the next wave
+                _isBetweenWaves = false;
+            }
+        }
+        else
+        {    
+            _spawnTimer -= Time.deltaTime;
+            if(_spawnTimer <= 0 && _spawnCounter < _currentWave.enemiesPerWave)
+            {
+                _spawnTimer = _currentWave.spawInterval;
+                SpawnEnemy();
+                _spawnCounter++;
+            }
+            else if(_spawnCounter >= _currentWave.enemiesPerWave &&
+                     _enemiesRemoved >= _currentWave.enemiesPerWave) // if the enemies were cleared all
+            {
+                _isBetweenWaves = true;
+                _waveCooldown = _timeBetweenWaves;
+            }
         }
     }
 
-    public void SpawnEnemy()
+    private void SpawnEnemy()
     {
-        GameObject spawnedObject = pool.GetPooledObject();
-        spawnedObject.transform.position = transform.position;
-        spawnedObject.SetActive(true);
+        if(_poolDictionary.TryGetValue(_currentWave.enemyType, out var pool))
+        {    
+            GameObject spawnedObject = pool.GetPooledObject();
+            spawnedObject.transform.position = transform.position;
+            spawnedObject.SetActive(true);
+        }
+    }
+
+    private void HandleEnemyReachedEnd(EnemyData data) // do it when enemy reached ends
+    {
+        _enemiesRemoved++;
+
+        Debug.Log(data.name);
     }
 }
